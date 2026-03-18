a68096dd3b422ee1b28c2f1a537d6dd73423c35d
pabloodiaz <136119885+pabloodiaz@users.noreply.github.com>
2026-03-18 11:32:45 -0300

    Update 18/3 Alojamiento.cpp

--- src/Alojamiento.cpp
+++ src/Alojamiento.cpp
@@ -3,31 +3,49 @@
 #include "Alojamiento.hpp"
 #include "TipoRegimen.hpp"
 
-Alojamiento::Alojamiento(std::string cod, std::string desc, int precio, DTFecha f, std::string h, TipoRegimen reg, int cant) :
-    Experiencia(cod, desc, precio, f), 
-    hotel(h),
-    regimen(reg),
-    cantNoches(cant)
-{}
+Alojamiento::Alojamiento(string cod,string desc, int precio, DTFecha f, string h, TipoRegimen reg, int cant) : Experiencia(cod, desc, precio, f){
+    hotel = h;
+    regimen = reg;
+    cantNoches = cant;
+}
+Alojamiento :: Alojamiento(Alojamiento& a): Experiencia(a.getCodigoReserva(),a.getDescripcion(),a.getPrecioBase(),a.getFecha()){
+    hotel = a.hotel;
+    regimen = a.regimen;
+    cantNoches = a.cantNoches;
+}
 
 Alojamiento::~Alojamiento() {}
 
-std::string getHotel() {
-    return this->hotel;
+string Alojamiento::getHotel() {
+    return hotel;
 }
 
-TipoRegimen getRegimen() {
-    return this->hotel
+TipoRegimen Alojamiento::getRegimen() {
+    return regimen;
 }
 
-int getCantNoches() {
-    return this->hotel;
+int Alojamiento::getCantNoches() {
+    return cantNoches;
 }
+
+void Alojamiento::setHotel(string h){
+    hotel = h;
+}
+
+void Alojamiento::setRegimen(TipoRegimen r){
+    regimen = r;
+}
+
+void Alojamiento::setCantNoches(int cant){
+    cantNoches = cant;
+}
+
 
 float Alojamiento::calcularCosto() {
-    if (this->regimen == TipoRegimen::AllInclusive) {
-        return ((getPrecioBase() + 10) * this->cantNoches);
-    } else {
-        return (getPrecioBase() * this->cantNoches);
+    if (regimen == TipoRegimen::AllInclusive) {
+        return getPrecioBase()*cantNoches + 10*cantNoches;
+    }
+    else{
+        return getPrecioBase() * cantNoches;
     }
 }
