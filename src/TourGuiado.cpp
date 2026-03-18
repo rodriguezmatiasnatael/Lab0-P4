d2d32c6d391f3930b8cb596c33cef2c2271a9667
pabloodiaz <136119885+pabloodiaz@users.noreply.github.com>
2026-03-18 12:26:23 -0300

    Update 2 18/3 TourGuiado.cpp

--- src/TourGuiado.cpp
+++ src/TourGuiado.cpp
@@ -5,22 +5,51 @@
 
 using namespace std;
 
-TourGuiado::TourGuiado(std::string cod, std::string desc, int precio, DTFecha f, std::string ag, const std::set<std::string>& lugares) : 
-    Experiencia(cod, desc, precio, f),
-    agencia(ag),
-    lugaresVisitados(lugares)
-{}
-
-TourGuiado::~TourGuiado() {}
-
-std::string getAgencia() {
-    return this->agencia
+TourGuiado::TourGuiado(string cod, string desc, int precio, DTFecha f, string ag, const set<string>& lugares,int cantL,int cantT) : Experiencia(cod, desc, precio, f){
+    agencia = ag;
+    lugaresVisitados = lugares;
+    cantLugares = cantL;
+    cantTuristas = cantT;
 }
 
-std::set<std::string> getLugaresVisitados() {
-    return this->lugaresVisitados;
+TourGuiado::TourGuiado(TourGuiado& tour): Experiencia(tour.getCodigoReserva(),tour.getDescripcion(),tour.getPrecioBase(),tour.getFecha()){
+    agencia = tour.agencia;
+    lugaresVisitados = tour.lugaresVisitados;
+    cantLugares = tour.cantLugares;
+    cantTuristas = tour.cantTuristas;
+}
+TourGuiado::~TourGuiado() {}
+
+string TourGuiado::getAgencia() {
+    return agencia;
+}
+
+set<string>& TourGuiado::getLugaresVisitados() {
+    return lugaresVisitados;
+}
+
+int TourGuiado::getCantLugares(){
+    return cantLugares;
+}
+
+int TourGuiado::getCantTuristas(){
+    return cantTuristas;
+}
+
+void TourGuiado::setAgencia(string a){
+    agencia = a;
+}
+void TourGuiado::setLugaresVisitados(const set<string>& lugares){
+    lugaresVisitados = lugares;
+}
+
+void TourGuiado::setCantLugares(int cant){
+    cantLugares = cant;
+}
+void TourGuiado::setCantTuristas(int cant){
+    cantTuristas = cant;
 }
 
 float TourGuiado :: calcularCosto(){
-    return 0;
+    return ((getPrecioBase()+2)*cantLugares*cantTuristas*;
 }
