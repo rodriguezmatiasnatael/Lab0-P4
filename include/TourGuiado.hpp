72204cb6f3581ea9bedbfeed4d5886191ba95d47
pabloodiaz <136119885+pabloodiaz@users.noreply.github.com>
2026-03-18 12:27:59 -0300

    Update 2 18/3 TourGuiado.hpp

--- include/TourGuiado.hpp
+++ include/TourGuiado.hpp
@@ -8,20 +8,25 @@ class TourGuiado : public Experiencia {
     private:
         string agencia;
         set<string> lugaresVisitados;
+        int cantLugares;
         int cantTuristas;
     public:
-        TourGuiado(string, string, int, DTFecha, string,set<string>&,int);
+        TourGuiado(string, string, int, DTFecha, string,set<string>&,int,int);
         TourGuiado(TourGuiado&);
         ~TourGuiado() override;
 
         string getAgencia();
         //aca no estoy seguro si pasarlo por referencia
         set<string>& getLugaresVisitados();
+        int getCantLugares();
+        int getCantTuristas();
 
         void setAgencia(string);
         //preguntar el &
         void setLugaresVisitados(const set<string>&);
+        void setCantLugares(int);
         void setCantTuristas(int);
+
         float calcularCosto() override;
 };
 
