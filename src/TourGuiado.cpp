85726e881b5d3e1f067d151028da19d2f7c3d0e2
pabloodiaz <136119885+pabloodiaz@users.noreply.github.com>
2026-03-20 16:40:23 -0300

    borre cosas y corregi la funcion calcularcosto

--- src/TourGuiado.cpp
+++ src/TourGuiado.cpp
@@ -1,49 +1,20 @@
 #include <iostream>
 #include <string>
-#include "TourGuiado.h"
-#include "Experiencia.h"
+#include "TourGuiado.hpp"
+#include "Experiencia.hpp"
 
 using namespace std;
 
-TourGuiado::TourGuiado(string cod, string desc, int precio, DTFecha f, string ag, const set<string>& lugares,int cantL,int cantT) : Experiencia(cod, desc, precio, f){
+TourGuiado::TourGuiado(std::string cod,std::string desc, int precio, DTFecha f, std::string ag,std::set<std::string> lugares,int cantL,int cantT) : Experiencia(cod, desc, precio, f){
     agencia = ag;
     lugaresVisitados = lugares;
-    cantLugares = cantL;
-    cantTuristas = cantT;
 }
+
 
 TourGuiado::~TourGuiado() {}
 
-string TourGuiado::getAgencia() {
-    return agencia;
-}
-
-set<string>& TourGuiado::getLugaresVisitados() {
-    return lugaresVisitados;
-}
-
-int TourGuiado::getCantLugares(){
-    return cantLugares;
-}
-
-int TourGuiado::getCantTuristas(){
-    return cantTuristas;
-}
-
-void TourGuiado::setAgencia(string a){
-    agencia = a;
-}
-void TourGuiado::setLugaresVisitados(const set<string>& lugares){
-    lugaresVisitados = lugares;
-}
-
-void TourGuiado::setCantLugares(int cant){
-    cantLugares = cant;
-}
-void TourGuiado::setCantTuristas(int cant){
-    cantTuristas = cant;
-}
 
 float TourGuiado :: calcularCosto(){
-    return ((getPrecioBase()+2)*cantLugares*cantTuristas*;
+    //preciobase + 2.cantlugares.cantturistas
+    return (getPrecioBase() + (2*lugaresVisitados.size()*getParticipantes().size()));
 }
