c04e5341276e786e5780b628727fd5ed05adc803
pabloodiaz <136119885+pabloodiaz@users.noreply.github.com>
2026-03-20 16:29:55 -0300

    borré los getters y setters ya que no se usan

--- include/Alojamiento.hpp
+++ include/Alojamiento.hpp
@@ -11,17 +11,8 @@ class Alojamiento : public Experiencia {
         int cantNoches;
     public:
         Alojamiento(std::string, std::string, int, DTFecha, std::string, TipoRegimen, int);
-        Alojamiento(Alojamiento&);
         ~Alojamiento() override;
         
-        std::string getHotel();
-        TipoRegimen getRegimen();
-        int getCantNoches();
-
-        void setHotel(std::string);
-        void setRegimen(TipoRegimen);
-        void setCantNoches(int);
-
         //no seria virtual??
         float virtual calcularCosto() override;
 };
